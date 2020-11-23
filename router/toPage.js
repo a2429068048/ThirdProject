980ee1e84ac4dd58e83d431c33f687dd43f27e9a
HJW <2453142525@qq.com>
2020-11-23 20:52:38 +0800

    首页完成2

--- router/toPage.js
+++ router/toPage.js
@@ -4,11 +4,53 @@ const exp = require("express");
 
 const router = exp.Router();
 
+const {Goods}=require('../db')
+
 // ------- 首页 -------
 router.get("/", (req, res) => {
     res.render("index");
 })
-
+// ---------限时团购---------
+router.get("/groupBuy", (req, res) => {
+    Goods.find({sort:"groupBuy"},(err,data)=>{
+        if (!err) {
+            res.send(data);
+        }else{
+            console.log(err);
+        }
+    })
+    
+})
+// ---------新品推荐---------
+router.get("/newpro", (req, res) => {
+    Goods.find({sort:"newpro"},(err,data)=>{
+        if (!err) {
+            res.send(data);
+        }else{
+            console.log(err);
+        }
+    })
+})
+// ---------热卖专区---------
+router.get("/hot", (req, res) => {
+    Goods.find({sort:"hot"},(err,data)=>{
+        if (!err) {
+            res.send(data);
+        }else{
+            console.log(err);
+        }
+    })
+})
+// ---------发现好物---------
+router.get("/goodThings", (req, res) => {
+    Goods.find({sort:"goodThings"},(err,data)=>{
+        if (!err) {
+            res.send(data);
+        }else{
+            console.log(err);
+        }
+    })
+})
 
 
 //------- 发现 ---------
