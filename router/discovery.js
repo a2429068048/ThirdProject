f24b8989c359d9c75fe08838e9dc15013d05ae5b
BlackCat <2429068048@qq.com>
2020-11-23 19:43:41 +0800

    发现-首页布局基本完成

---
+++ router/discovery.js
@@ -0,0 +1,23 @@
+const exp = require("express");
+
+const router = exp.Router();
+
+router.get("/index", (req, res) => {
+    res.render("community");
+})
+
+
+router.get("/shop", (req, res) => {
+    res.redirect("/");
+})
+
+router.get("/message",(req, res) => {
+    res.render("message");
+})
+
+router.get("/me", (req,  res) => {
+    res.render("me");
+})
+
+
+module.exports = router;
