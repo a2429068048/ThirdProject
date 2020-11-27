f2a5ae9d1024bacffa7e195063305eb0c3ec4920
BlackCat <2429068048@qq.com>
2020-11-27 23:12:33 +0800

    添加（完善），删除功能完成，修改页面渲染成功----管理员

--- router/manager.js
+++ router/manager.js
@@ -1,5 +1,5 @@
 // 管理员的路由
-const {Goods, ShopCar} = require("../db");
+const {Goods, ShopCar, Topic} = require("../db");
 const multer = require("multer");
 const exp = require("express");
 const fs = require("fs");
@@ -12,18 +12,37 @@ router.get("/", (req, res) => {
     res.render("manage/indexM");
 })
 
-router.get("/addGoods", (req, res) => {
-    res.render("manage/goodsInfo", {
-        title:"商品添加",
-        goods:{}
-    });
+router.get("/shopInfo", (req, res) => {
+   if (req.query.id) {
+        Goods.findOne({_id:req.query.id}, (err, data) => {
+            if (!err){
+                if (data){
+                    res.render("manage/goodsInfo", {
+                        title: "商品修改",
+                        goods:data
+                    })
+                } 
+            } else {
+                
+                console.log("商品表展示失败");
+                console.log(req.query.id)
+                console.log(err);
+            }
+        })
+   } else {
+        res.render("manage/goodsInfo", {
+            title:"商品添加",
+            goods:{}
+        });
+   }
+   
 })
 
 // 定义商品图片的文件夹
 let storage = multer.diskStorage({
     destination:"static/shops",
     filename:function (req, file, cb) {
-        console.log(file);
+        // console.log(file);
         let index = file.originalname.lastIndexOf(".");
         let time = Date.now();
         let filename = time + file.originalname.slice(index);
@@ -78,6 +97,56 @@ router.post("/goodsImgArrtwo", upload.array("imgArrtwo"), (req, res) => {
             sendInfo(res, 1, "添加成功",{id : data.id});
         }
     })
+})
+
+// 查看商品
+router.get("/goodsInfo", (req, res) => {
+    Goods.find((err, data) => {
+        if (!err) {
+            res.render("./manage/allGoods", {data});
+        } else {
+            console.log("商品列表获取失败---goodsInfo");
+            console.log(err);
+        }
+    })
+})
+// 删除商品
+router.get("/deleteGood", (req, res) => {
+    Goods.findOne({_id: req.query.id}, (err, data) => {
+        if (!err) {
+            // 删除文件
+            fs.unlink("./static" + data.imgSrc, err => err && console.log(err));
+            if (data.imgArr && data.imgArr.length) {
+                data.imgArr.forEach(img => {
+                    fs.unlink("./static" + img, (err) => err && console.log(err))
+                }) 
+            }
+            if (data.imgArrtwo && data.imgArrtwo.length) {
+                data.imgArrtwo.forEach(img => {
+                    fs.unlink("./static" + img, (err) => err && console.log(err))
+                }) 
+            }
+            Goods.deleteOne(data, err => {
+                if (!err) {
+                    // 删除购物车内容
+                    
+                    sendInfo(res, 1, "删除成功");
+                } else {
+                    console.log("用户表删除失败---deleteGood")
+                    console.log(err);
+                }
+            });
+        
+        } else {
+            console.log("商品信息表获取失败---deleteGood");
+            console.log(err);
+        }
+    })
+})
+
+// 修改商品
+router.get("/modifyGood", (req, res) => {
+
 })
 
 function sendInfo(res, coder=0, msg="", data=null) {
